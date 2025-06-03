d7c4dfb5b08518b3655ac6b159cc4113afd83921
abugaberd <david.abugaber@gmail.com>
2025-06-03 17:52:34 -0500

    changed sidebar icon for generating a document

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex, Icon, Text, Accordion } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link as RouterLink } from "@tanstack/react-router"
-import { FiBriefcase, FiHome, FiSettings, FiUsers, FiBookOpen, FiClipboard, FiTool, FiPackage, FiFileText } from "react-icons/fi"
+import { FiBriefcase, FiHome, FiSettings, FiUsers, FiBookOpen, FiClipboard, FiTool, FiPackage, FiFilePlus } from "react-icons/fi"
 import { FaBalanceScale } from "react-icons/fa";
 import { TbPlugConnected } from "react-icons/tb";
 import type { IconType } from "react-icons/lib"
@@ -21,7 +21,7 @@ const categories = [
     icon: FiTool,
     items: [
       { icon: FiClipboard, title: "Review a document", path: "/veradoc" },
-      { icon: FiFileText, title: "Generate a document", path: "/reportgenie" },
+      { icon: FiFilePlus, title: "Generate a document", path: "/reportgenie" },
       { icon: FaBalanceScale, title: "Compare documents", path: "/twincheck" },
       { icon: TbPlugConnected, title: "Match fields across documents", path: "/formconnect" },
     ]
