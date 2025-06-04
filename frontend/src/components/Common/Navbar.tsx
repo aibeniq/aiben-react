f206d7a44f7ae86bb790853cf22cfa7bc09fa754
tobenik <tobias.nikko@gmail.com>
2025-06-04 14:42:47 -0700

    logo idea

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Flex, Image, useBreakpointValue } from "@chakra-ui/react"
 import { Link } from "@tanstack/react-router"
 
-import Logo from "/assets/images/aiben-logo-horizontal.png"
+// import Logo from "/assets/images/aiben-logo-new.png"
 import UserMenu from "./UserMenu"
 
 function Navbar() {
