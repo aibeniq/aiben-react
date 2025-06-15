5c9d3729884e0d52ef719f35a042bd365696a321
tobenik <tobias.nikko@gmail.com>
2025-06-15 14:59:43 -0700

    separate dev & prod environment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,24 @@
 import path from "node:path"
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  plugins: [react(), TanStackRouterVite()],
+    plugins: [react(), TanStackRouterVite()],
+    // Expose env variables to your app
+    define: {
+      "process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+    },
+  }
 })
